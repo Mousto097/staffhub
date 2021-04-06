771f99a1053853a743a8e13e453f13dd9234958f
Mamadou Bah <bmama067@uottawa.ca>
2021-04-05 21:06:58 -0400

    Added SCSS files

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -1,14 +1,14 @@
-import './App.scss';
 import { Switch, Route } from "react-router";
 import { BrowserRouter as Router } from "react-router-dom";
 import ScreenUrls from "./utils/screenUtils";
 import { Landing, Login } from "./pages";
 import AppRouter from "./AppRouter";
+import './App.scss';
 
 function App() {
 
   const AppLayout = () => (
-    <div id="app">
+    <div id="app" className="app-layout">
       <Switch>
         <Route path={ScreenUrls.APP_ROUTER} component={AppRouter} />
         <Route path={ScreenUrls.LOGIN} component={Login} />
